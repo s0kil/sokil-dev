f968efb60002143d23cb52926d11dcb1dd7a3da1
Daniel Sokil <dtechtours@gmail.com>
2021-06-11 13:04:12 -0400

    Fix polka Dep

--- rollup.config.js
+++ rollup.config.js
@@ -29,6 +29,7 @@ export default {
     output: config.client.output(),
     plugins: [
       replace({
+        preventAssignment: true,
         "process.browser": true,
         "process.env.NODE_ENV": JSON.stringify(mode),
       }),
@@ -60,6 +61,7 @@ export default {
     output: config.server.output(),
     plugins: [
       replace({
+        preventAssignment: true,
         "process.browser": false,
         "process.env.NODE_ENV": JSON.stringify(mode),
       }),
@@ -95,6 +97,7 @@ export default {
     plugins: [
       resolve(),
       replace({
+        preventAssignment: true,
         "process.browser": true,
         "process.env.NODE_ENV": JSON.stringify(mode),
       }),
